a1426e64fb3aeb7fde1206148857ed4757539a7d
Yuki-jukjis <taka0618.m@gmail.com>
2020-05-11 16:48:03 +0900

    classにしてvaluationはなんとかなった

--- main.js
+++ main.js
@@ -1,41 +1,317 @@
 "use strict";
-const a = (w) => ["t", w[1] === "a"];
+class Truth {
+    constructor(value) {
+        this.type = "t";
+        this.value = value;
+    }
+}
+;
+class Entity {
+    constructor(id) {
+        this.type = "e";
+        this.id = id;
+    }
+}
+;
+class Situation {
+    constructor(id) {
+        this.type = "s";
+        this.id = id;
+    }
+}
+;
+class ComplexValue {
+    constructor(type, value) {
+        this.type = type;
+        this.value = value;
+    }
+}
+class Variable {
+    constructor(name, type) {
+        this.sort = "variable";
+        this.name = name;
+        this.type = type;
+    }
+    ;
+    valuation(m, w, g) {
+        return g(this);
+    }
+    ;
+    toString() {
+        return this.name;
+    }
+    ;
+}
+class Constant {
+    constructor(name, type, interpretation) {
+        this.sort = "constant";
+        this.name = name;
+        this.type = type;
+        this.interpretation = interpretation;
+    }
+    ;
+    valuation(m, w, g) {
+        return this.interpretation(w);
+    }
+    toString() {
+        return this.name;
+    }
+    ;
+}
+class Not {
+    constructor(expr) {
+        this.sort = "￢";
+        this.type = "t";
+        this.expr = expr;
+    }
+    ;
+    valuation(m, w, g) {
+        return new Truth(!this.expr.valuation(m, w, g).value);
+    }
+    ;
+    toString() {
+        return "￢" + this.expr.toString();
+    }
+}
+class And {
+    constructor(left, right) {
+        this.sort = "∧";
+        this.type = "t";
+        this.left = left;
+        this.right = right;
+    }
+    ;
+    valuation(m, w, g) {
+        return new Truth(this.left.valuation(m, w, g).value && this.right.valuation(m, w, g).value);
+    }
+    ;
+    toString() {
+        return this.left.toString() + "∧" + this.right.toString();
+    }
+    ;
+}
+class Or {
+    constructor(left, right) {
+        this.sort = "∨";
+        this.type = "t";
+        this.left = left;
+        this.right = right;
+    }
+    ;
+    valuation(m, w, g) {
+        return new Truth(this.left.valuation(m, w, g).value || this.right.valuation(m, w, g).value);
+    }
+    ;
+    toString() {
+        return this.left.toString() + "∨" + this.right.toString();
+    }
+    ;
+}
+class If {
+    constructor(left, right) {
+        this.sort = "⇒";
+        this.type = "t";
+        this.left = left;
+        this.right = right;
+    }
+    ;
+    valuation(m, w, g) {
+        return new Truth(!this.left.valuation(m, w, g).value || this.right.valuation(m, w, g).value);
+    }
+    ;
+    toString() {
+        return this.left.toString() + "⇒" + this.right.toString();
+    }
+    ;
+}
+class Iff {
+    constructor(left, right) {
+        this.sort = "⇔";
+        this.type = "t";
+        this.left = left;
+        this.right = right;
+    }
+    ;
+    valuation(m, w, g) {
+        return new Truth(this.left.valuation(m, w, g).value == this.right.valuation(m, w, g).value);
+    }
+    ;
+    toString() {
+        return this.left.toString() + "⇔" + this.right.toString();
+    }
+    ;
+}
+class Exist {
+    constructor(variable, expr) {
+        this.sort = "∃";
+        this.type = "t";
+        this.variable = variable;
+        this.expr = expr;
+    }
+    ;
+    valuation(m, w, g) {
+        return new Truth(m.interpretationDomain(this.variable.type).some(value => this.expr.valuation(m, w, assign(g, this.variable, value)).value));
+    }
+    ;
+    toString() {
+        return "∃" + this.variable.toString() + "." + this.expr.toString();
+    }
+    ;
+}
+class All {
+    constructor(variable, expr) {
+        this.sort = "∀";
+        this.type = "t";
+        this.variable = variable;
+        this.expr = expr;
+    }
+    ;
+    valuation(m, w, g) {
+        return new Truth(m.interpretationDomain(this.variable.type).every(value => this.expr.valuation(m, w, assign(g, this.variable, value)).value));
+    }
+    ;
+    toString() {
+        return "∀" + this.variable.toString() + "." + this.expr.toString();
+    }
+    ;
+}
+class Equal {
+    constructor(left, right) {
+        this.sort = "＝";
+        this.type = "t";
+        this.left = left;
+        this.right = right;
+    }
+    ;
+    valuation(m, w, g) {
+        return new Truth(equals(m, this.left.valuation(m, w, g), this.left.valuation(m, w, g)));
+    }
+    toString() {
+        return this.left.toString() + "＝" + this.right.toString();
+    }
+    ;
+}
+class Must {
+    constructor(expr) {
+        this.sort = "□";
+        this.type = "t";
+        this.expr = expr;
+    }
+    valuation(m, w, g) {
+        return new Truth(m.worlds.every(_w => this.expr.valuation(m, _w, g).value));
+    }
+    toString() {
+        return "□" + this.expr.toString();
+    }
+    ;
+}
+class May {
+    constructor(expr) {
+        this.sort = "◇";
+        this.type = "t";
+        this.expr = expr;
+    }
+    valuation(m, w, g) {
+        return new Truth(m.worlds.some(_w => this.expr.valuation(m, _w, g).value));
+    }
+    toString() {
+        return "◇" + this.expr.toString();
+    }
+    ;
+}
+class Up {
+    constructor(expr, type) {
+        this.sort = "↑";
+        this.type = ["s", type];
+        this.expr = expr;
+    }
+    valuation(m, w, g) {
+        return new ComplexValue(this.type, _w => this.expr.valuation(m, _w, g));
+    }
+    toString() {
+        return "↑" + this.expr.toString();
+    }
+    ;
+}
+class Down {
+    constructor(expr, type) {
+        this.sort = "↓";
+        this.type = type;
+        this.expr = expr;
+    }
+    valuation(m, w, g) {
+        return apply(this.expr.valuation(m, w, g), w);
+    }
+    toString() {
+        return "↓" + this.expr.toString();
+    }
+    ;
+}
+class Lamda {
+    constructor(variable, expr, type) {
+        this.sort = "λ";
+        this.variable = variable;
+        this.type = type;
+        this.expr = expr;
+    }
+    ;
+    valuation(m, w, g) {
+        return new ComplexValue(this.type, d => this.expr.valuation(m, w, assign(g, this.variable, d)));
+    }
+    toString() {
+        return "λ" + this.variable.toString() + "." + this.expr.toString();
+    }
+    ;
+}
+class Apply {
+    constructor(left, right, type) {
+        this.sort = "apply";
+        this.left = left;
+        this.right = right;
+        this.type = type;
+    }
+    valuation(m, w, g) {
+        return apply(this.left.valuation(m, w, g), (this.right.valuation(m, w, g)));
+    }
+    toString() {
+        return this.left.toString() + "(" + this.right.toString() + ")";
+    }
+    ;
+}
+function equals(m, a, b) {
+    if (a.type === "t")
+        return b.type === "t" && a.value === b.value;
+    if (a.type === "e")
+        return b.type === "e" && a.id === b.id;
+    if (a.type === "s")
+        return b.type === "s" && a.id === b.id;
+    if (b.type === "t" || b.type === "e" || b.type === "s")
+        return false;
+    return m.interpretationDomain(a.type[0]).every(x => equals(m, apply(a, x), apply(b, x)));
+}
+function apply(func, x) {
+    return func.value(x);
+}
+class Model {
+    constructor(domain, worlds) {
+        this.domain = domain;
+        this.worlds = worlds;
+    }
+    interpretationDomain(type) {
+        if (type === "t")
+            return [new Truth(true), new Truth(false)];
+        if (type === "e")
+            return [...this.domain];
+        if (type === "s")
+            return [...this.worlds];
+        const aa = this.interpretationDomain(type[0]);
+        const bb = this.interpretationDomain(type[1]);
+        // bbのaa.lengthタプル（全組み合わせ）の配列を作る
+        const table = aa.reduce((prev) => bb.map(b => prev.map(t => [b, ...t, b])).reduce((a, b) => a.concat(b), []), [[]]);
+        return table.map(t => new ComplexValue(type, (x) => t[aa.findIndex(y => equals(this, x, y))]));
+    }
+}
 // g_[x/d]
 function assign(g, variable, value) {
     return v => v.name === variable.name ? value : g(v);
-}
-function isTruthME(me) {
-    return me.sort === "￢" || me.sort === "∧" || me.sort === "∨";
-    /* Or ? Truth :
-    T extends If ? Truth :
-    T extends Iff ? Truth :
-    T extends Exist<Type> ? Truth :
-    T extends All<Type> ? Truth :
-    T extends Equal<Type> ? Truth :
-    T extends Must ? Truth :
-    T extends May ? Truth :;*/
-}
-function valuation(me, m, w, g) {
-    switch (me.sort) {
-        case "constant": return me.interpretation(w);
-        case "variable": return g(me);
-        case "λ":
-            return (d) => valuation(me.expr, m, w, assign(g, me.variable, d));
-            ;
-        case "apply": return valuation(me.left, m, w, g)(valuation(me.right, m, w, g));
-        case "＝": return ["t", valuation(me.left, m, w, g) === valuation(me.right, m, w, g)];
-        case "￢": return ["t", !valuation(me.expr, m, w, g)[1]];
-        case "∧": return ["t", valuation(me.left, m, w, g)[1] && valuation(me.right, m, w, g)[1]];
-        case "∨": return ["t", valuation(me.left, m, w, g)[1] || valuation(me.right, m, w, g)[1]];
-        case "⇒": return ["t", !valuation(me.left, m, w, g)[1] || valuation(me.right, m, w, g)[1]];
-        case "⇔": return ["t", valuation(me.left, m, w, g) === valuation(me.right, m, w, g)];
-        case "∃": return ["t", Array.from(m.domein).some(value => valuation(me.expr, m, w, assign(g, me.variable, value)))];
-        case "∀": return ["t", Array.from(m.domein).every(value => valuation(me.expr, m, w, assign(g, me.variable, value)))];
-        case "◇": return ["t", Array.from(m.worlds).every(_w => valuation(me.expr, m, _w, g))];
-        case "□": return ["t", Array.from(m.worlds).some(_w => valuation(me.expr, m, _w, g))];
-        case "↑": return (_w) => valuation(me.expr, m, _w, g);
-        case "↓": return valuation(me.expr, m, w, g)(w);
-        // 網羅チェック
-        default: return me;
-    }
 }
--- main.ts
+++ main.ts
@@ -1,95 +1,343 @@
-type Truth = ["t", boolean];
-type Entity = ["e", string];
-type Situation = ["s", string];
+class Truth {
+    readonly type = "t";
+    readonly value: boolean;
+    constructor(value: boolean) {
+        this.value = value;
+    }
+};
+class Entity {
+    readonly type = "e";
+    readonly id: string;
+    constructor(id: string) {
+        this.id = id;
+    }
+};
+class Situation {
+    readonly type = "s";
+    readonly id: string;
+    constructor(id: string) {
+        this.id = id;
+    }
+};
+class ComplexValue<A extends Type, B extends Type> {
+    readonly type: [A, B];
+    readonly value: (_: TypeValue<A>) => TypeValue<B>;
+    constructor(type: [A, B], value: (_: TypeValue<A>) => TypeValue<B>) {
+        this.type = type;
+        this.value = value;
+    }
+}
 
 type Type = "t" | "e" | "s" | [Type, Type];
 
-const a: TypeValue<["s", "t"]> = (w: Situation) => ["t", w[1]==="a"];
+type TypeValue<T extends Type> =
+    T extends "t" ? Truth :
+    T extends "e" ? Entity :
+    T extends "s" ? Situation :
+    T extends [Type, Type] ? ComplexValue<T[0], T[1]> :
+    never;
 
-type ME<A extends Type> = 
-    (A extends "t" ? Not : never) |
-    (A extends "t" ? And : never) |
-    (A extends "t" ? Or : never) |
-    (A extends "t" ? If : never) |
-    (A extends "t" ? Iff : never) |
-    (A extends "t" ? Exist<Type> : never) |
-    (A extends "t" ? All<Type> : never) |
-    (A extends "t" ? Equal<Type> : never) |
-    (A extends "t" ? Must : never) |
-    (A extends "t" ? May : never) |
-    (A extends ["s", Type] ? Up<A[1]> : never) |
-    (A extends [Type, Type] ? Lamda<A[0], A[1]> : never)
-    | Variable<A> | Constant<A>| Apply<Type, A> | Down<A>;
+type Assignment = <B extends Type>(_: Variable<B>) => TypeValue<B>;
 
-interface Variable<A extends Type> { sort: "variable"; name: string; type: A }
-interface Constant<A extends Type> { sort: "constant"; name: string; interpretation: (w: Situation) => TypeValue<A>; }
-interface Not { sort: "￢"; expr: ME<"t">; }
-interface And { sort: "∧"; left: ME<"t">; right: ME<"t">; }
-interface Or { sort: "∨"; left: ME<"t">; right: ME<"t">; }
-interface If { sort: "⇒"; left: ME<"t">; right: ME<"t">; }
-interface Iff { sort: "⇔"; left: ME<"t">; right: ME<"t">; }
-interface Exist<A extends Type> { sort: "∃"; variable: Variable<A>; expr: ME<"t">; }
-interface All<A extends Type> { sort: "∀"; variable: Variable<A>; expr: ME<"t">; }
-interface Equal<A extends Type> { sort: "＝"; left: ME<A>; right: ME<A>; }
-interface Must { sort: "□"; expr: ME<"t">; }
-interface May { sort: "◇"; expr: ME<"t">; }
-interface Up<A extends Type> { sort: "↑"; expr: ME<A>; }
-interface Down<A extends Type> { sort: "↓"; expr: ME<["s", A]>; }
-interface Lamda<A extends Type, B extends Type> { sort: "λ"; variable: Variable<A>, expr: ME<B>; }
-interface Apply<A extends Type, B extends Type> { sort: "apply"; left: ME<[A, B]>; right: ME<A>; }
-
-interface Model {
-    domein: Set<Entity>;
-    worlds: Set<Situation>;
+interface ME<A extends Type> {
+    type: Type;
+    valuation: (m: Model, w: Situation, g: Assignment) => TypeValue<A>;
 }
 
+class Variable<A extends Type> implements ME<A> {
+    readonly sort = "variable";
+    readonly name: string;
+    readonly type: A;
+    constructor(name: string, type: A) {
+        this.name = name;
+        this.type = type;
+    };
+    valuation(m: Model, w: Situation, g: Assignment): TypeValue<A> {
+        return g(this);
+    };
+    toString() {
+        return this.name;
+    };
+}
+class Constant<A extends Type> implements ME<A> {
+    readonly sort = "constant";
+    readonly name: string;
+    readonly type: A;
+    interpretation: (w: Situation) => TypeValue<A>;
+    constructor(name: string, type: A, interpretation: (w: Situation) => TypeValue<A>) {
+        this.name = name;
+        this.type = type;
+        this.interpretation = interpretation;
+    };
+    valuation(m: Model, w: Situation, g: Assignment): TypeValue<A> {
+        return this.interpretation(w);
+    }
+    toString() {
+        return this.name;
+    };
+}
+class Not implements ME<"t"> {
+    readonly sort = "￢";
+    readonly type = "t";
+    readonly expr: ME<"t">;
+    constructor(expr: ME<"t">) {
+        this.expr = expr;
+    };
+    valuation(m: Model, w: Situation, g: Assignment): Truth {
+        return new Truth(!this.expr.valuation(m, w, g).value);
+    };
+    toString() {
+        return "￢" + this.expr.toString();
+    }
+}
+class And implements ME<"t"> {
+    readonly sort = "∧";
+    readonly type = "t";
+    readonly left: ME<"t">;
+    readonly right: ME<"t">;
+    constructor(left: ME<"t">, right: ME<"t">) {
+        this.left = left;
+        this.right = right;
+    };
+    valuation(m: Model, w: Situation, g: Assignment): Truth {
+        return new Truth(this.left.valuation(m, w, g).value && this.right.valuation(m, w, g).value);
+    };
+    toString() {
+        return this.left.toString() + "∧" + this.right.toString();
+    };
+}
+class Or implements ME<"t"> {
+    readonly sort = "∨";
+    readonly type = "t";
+    readonly left: ME<"t">;
+    readonly right: ME<"t">;
+    constructor(left: ME<"t">, right: ME<"t">) {
+        this.left = left;
+        this.right = right;
+    };
+    valuation(m: Model, w: Situation, g: Assignment): Truth {
+        return new Truth(this.left.valuation(m, w, g).value || this.right.valuation(m, w, g).value);
+    };
+    toString() {
+        return this.left.toString() + "∨" + this.right.toString();
+    };
+}
+class If implements ME<"t"> {
+    readonly sort = "⇒";
+    readonly type = "t";
+    readonly left: ME<"t">;
+    readonly right: ME<"t">;
+    constructor(left: ME<"t">, right: ME<"t">) {
+        this.left = left;
+        this.right = right;
+    };
+    valuation(m: Model, w: Situation, g: Assignment): Truth {
+        return new Truth(!this.left.valuation(m, w, g).value || this.right.valuation(m, w, g).value);
+    };
+    toString() {
+        return this.left.toString() + "⇒" + this.right.toString();
+    };
+}
+class Iff implements ME<"t"> {
+    readonly sort = "⇔";
+    readonly type = "t";
+    readonly left: ME<"t">;
+    readonly right: ME<"t">;
+    constructor(left: ME<"t">, right: ME<"t">) {
+        this.left = left;
+        this.right = right;
+    };
+    valuation(m: Model, w: Situation, g: Assignment): Truth {
+        return new Truth(this.left.valuation(m, w, g).value == this.right.valuation(m, w, g).value);
+    };
+    toString() {
+        return this.left.toString() + "⇔" + this.right.toString();
+    };
+}
+class Exist<A extends Type> implements ME<"t"> {
+    readonly sort = "∃";
+    readonly type = "t";
+    readonly variable: Variable<A>;
+    readonly expr: ME<"t">;
+    constructor(variable: Variable<A>, expr: ME<"t">) {
+        this.variable = variable;
+        this.expr = expr;
+    };
+    valuation(m: Model, w: Situation, g: Assignment): Truth {
+        return new Truth(m.interpretationDomain(this.variable.type).some(value => this.expr.valuation(m, w, assign(g, this.variable, value)).value));
+    };
+    toString() {
+        return "∃" + this.variable.toString() + "." + this.expr.toString();
+    };
+}
+class All<A extends Type> implements ME<"t"> {
+    readonly sort = "∀";
+    readonly type = "t";
+    readonly variable: Variable<A>;
+    readonly expr: ME<"t">;
+    constructor(variable: Variable<A>, expr: ME<"t">) {
+        this.variable = variable;
+        this.expr = expr;
+    };
+    valuation(m: Model, w: Situation, g: Assignment): Truth {
+        return new Truth(m.interpretationDomain(this.variable.type).every(value => this.expr.valuation(m, w, assign(g, this.variable, value)).value));
+    };
+    toString() {
+        return "∀" + this.variable.toString() + "." + this.expr.toString();
+    };
+}
+class Equal<A extends Type> implements ME<"t"> {
+    readonly sort = "＝";
+    readonly type = "t";
+    readonly left: ME<A>;
+    readonly right: ME<A>;
+    constructor(left: ME<A>, right: ME<A>) {
+        this.left = left;
+        this.right = right;
+    };
+    valuation(m: Model, w: Situation, g: Assignment): Truth {
+        return new Truth(equals(m, this.left.valuation(m, w, g), this.left.valuation(m, w, g)));
+    }
+    toString() {
+        return this.left.toString() + "＝" + this.right.toString();
+    };
+}
+class Must implements ME<"t"> {
+    readonly sort = "□";
+    readonly type = "t";
+    readonly expr: ME<"t">;
+    constructor(expr: ME<"t">) {
+        this.expr = expr;
+    }
+    valuation(m: Model, w: Situation, g: Assignment): Truth {
+        return new Truth(m.worlds.every(_w => this.expr.valuation(m, _w, g).value));
+    }
+    toString() {
+        return "□" + this.expr.toString();
+    };
+}
+class May implements ME<"t"> {
+    readonly sort = "◇";
+    readonly type = "t";
+    readonly expr: ME<"t">;
+    constructor(expr: ME<"t">) {
+        this.expr = expr;
+    }
+    valuation(m: Model, w: Situation, g: Assignment): Truth {
+        return new Truth(m.worlds.some(_w => this.expr.valuation(m, _w, g).value));
+    }
+    toString() {
+        return "◇" + this.expr.toString();
+    };
+}
+class Up<A extends Type> implements ME<["s", A]> {
+    readonly sort = "↑";
+    readonly type: ["s", A];
+    readonly expr: ME<A>;
+    constructor(expr: ME<A>, type: A) {
+        this.type = ["s", type];
+        this.expr = expr;
+    }
+    valuation(m: Model, w: Situation, g: Assignment): TypeValue<["s", A]> {
+        return new ComplexValue(this.type, _w => this.expr.valuation(m, _w, g));
+    }
+    toString() {
+        return "↑" + this.expr.toString();
+    };
+}
+class Down<A extends Type> implements ME<A> {
+    readonly sort = "↓";
+    readonly type: A;
+    readonly expr: ME<["s", A]>;
+    constructor(expr: ME<["s", A]>, type: A) {
+        this.type = type;
+        this.expr = expr;
+    }
+    valuation(m: Model, w: Situation, g: Assignment): TypeValue<A> {
+        return apply(this.expr.valuation(m, w, g), w);
+    }
+    toString() {
+        return "↓" + this.expr.toString();
+    };
+}
+class Lamda<A extends Type, B extends Type> implements ME<[A, B]> {
+    readonly sort = "λ";
+    readonly type: [A, B];
+    readonly variable: Variable<A>;
+    readonly expr: ME<B>;
+    constructor(variable: Variable<A>, expr: ME<B>, type: [A, B]) {
+        this.variable = variable;
+        this.type = type;
+        this.expr = expr;
+    };
+    valuation(m: Model, w: Situation, g: Assignment): TypeValue<[A, B]> {
+        return new ComplexValue(this.type, d => this.expr.valuation(m, w, assign(g, this.variable, d)));
+    }
+    toString() {
+        return "λ" + this.variable.toString() + "." + this.expr.toString();
+    };
+}
+class Apply<A extends Type, B extends Type> implements ME<B> {
+    readonly sort = "apply";
+    readonly type: B;
+    readonly left: ME<[A, B]>;
+    readonly right: ME<A>;
+    constructor(left: ME<[A, B]>, right: ME<A>, type: B) {
+        this.left = left;
+        this.right = right;
+        this.type = type;
+    }
+    valuation(m: Model, w: Situation, g: Assignment): TypeValue<B> {
+        return apply(this.left.valuation(m, w, g), (this.right.valuation(m, w, g)));
+    }
+    toString() {
+        return this.left.toString() + "(" + this.right.toString() + ")";
+    };
+}
+
+function equals(m: Model, a: TypeValue<Type>, b: TypeValue<Type>): boolean {
+    if (a.type === "t")
+        return b.type === "t" && a.value === b.value;
+    if (a.type === "e")
+        return b.type === "e" && a.id === b.id;
+    if (a.type === "s")
+        return b.type === "s" && a.id === b.id;
+
+    if (b.type === "t" || b.type === "e" || b.type === "s") return false;
+
+    return m.interpretationDomain(a.type[0]).every(x => equals(m, apply(a, x), apply(b, x)));
+}
+
+function apply<A extends Type, B extends Type>(func: ComplexValue<A, B>, x: TypeValue<A>) {
+    return func.value(x);
+}
+
+class Model {
+    readonly domain: Entity[];
+    readonly worlds: Situation[];
+    constructor(domain: Entity[], worlds: Situation[]) {
+        this.domain = domain;
+        this.worlds = worlds;
+    }
+    interpretationDomain<A extends Type>(type: A): TypeValue<A>[] {
+        if (type === "t") return [new Truth(true), new Truth(false)];
+        if (type === "e") return [...this.domain];
+        if (type === "s") return [...this.worlds];
+
+        const aa: TypeValue<A[0]>[] = this.interpretationDomain(type[0]);
+        const bb: TypeValue<A[1]>[] = this.interpretationDomain(type[1]);
+
+        // bbのaa.lengthタプル（全組み合わせ）の配列を作る
+        const table: TypeValue<A[1]>[][] = aa.reduce((prev: TypeValue<A[1]>[][]) => bb.map(b => prev.map(t => [b, ...t, b])).reduce((a, b) => a.concat(b), []), [[]]);
+
+        return table.map(t => new ComplexValue(type, (x: TypeValue<A[0]>): TypeValue<A[1]> => t[aa.findIndex(y => equals(this, x, y))]));
+    }
+}
 
 // g_[x/d]
 function assign<A extends Type>(
-    g: <B extends Type>(_: Variable<B>) => TypeValue<B>,
+    g: Assignment,
     variable: Variable<A>,
-    value: TypeValue<A>): <B extends Type>(_: Variable<B>) => TypeValue<B> {
+    value: TypeValue<A>): Assignment {
     return v => v.name === variable.name ? value : g(v);
-}
-
-function isTruthME(me: ME<Type>): me is ME<"t"> {
-    return me.sort === "￢" || me.sort === "∧" || me.sort === "∨";
-    /* Or ? Truth : 
-    T extends If ? Truth : 
-    T extends Iff ? Truth : 
-    T extends Exist<Type> ? Truth :
-    T extends All<Type> ? Truth :
-    T extends Equal<Type> ? Truth :
-    T extends Must ? Truth :
-    T extends May ? Truth :;*/
-} 
-
-type TypeValue<T extends Type> =
-    T extends "t" ? Truth : 
-    T extends "e" ? Entity : 
-    T extends "s" ? Situation : 
-    T extends [Type, Type] ? (_:TypeValue<T[0]>)=>TypeValue<T[1]> : 
-    never;
-
-function valuation<A extends Type>(me: ME<A>, m: Model, w: Situation, g: <B extends Type>(_: Variable<B>) => TypeValue<B>): TypeValue<A> {
-    switch (me.sort) {
-        case "constant": return me.interpretation(w);
-        case "variable": return g(me);
-        case "λ": return (d) => valuation(me.expr, m, w, assign(g, me.variable, d));;
-        case "apply": return valuation(me.left, m, w, g)(valuation(me.right, m, w, g));
-        case "＝": return ["t", valuation<B>(me.left, m, w, g) === valuation<B>(me.right, m, w, g)];
-        case "￢": return ["t", !valuation<"t">(me.expr, m, w, g)[1]];
-        case "∧": return ["t", valuation<"t">(me.left, m, w, g)[1] && valuation<"t">(me.right, m, w, g)[1]];
-        case "∨": return ["t", valuation<"t">(me.left, m, w, g)[1] || valuation<"t">(me.right, m, w, g)[1]];
-        case "⇒": return ["t", !valuation<"t">(me.left, m, w, g)[1] || valuation<"t">(me.right, m, w, g)[1]];
-        case "⇔": return ["t", valuation<"t">(me.left, m, w, g) === valuation<"t">(me.right, m, w, g)];
-        case "∃": return ["t", Array.from(m.domein).some(value => valuation<"t">(me.expr, m, w, assign(g, me.variable, value)))];
-        case "∀": return ["t", Array.from(m.domein).every(value => valuation<"t">(me.expr, m, w, assign(g, me.variable, value)))];
-        case "◇": return ["t", Array.from(m.worlds).every(_w => valuation<"t">(me.expr, m, _w, g))];
-        case "□": return ["t", Array.from(m.worlds).some(_w => valuation<"t">(me.expr, m, _w, g))];
-        case "↑": return (_w:Situation) => valuation(me.expr, m, _w, g);
-        case "↓": return valuation(me.expr, m, w, g)(w);
-        // 網羅チェック
-        default: return me;
-    }
 }
